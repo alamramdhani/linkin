719e40057726816f6e3a56b04583dfc0d350f09f
lamz <43467384+alamramdhani@users.noreply.github.com>
2019-12-18 15:17:36 +0700

    Add files via upload

---
+++ linkin/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <title>Linkin</title>
+</head>
+<body>
+    <div class="text-center img-fluid container pt-3">
+        <img src="bunga.jfif" class="rounded-circle"  alt="Cinque Terre" width="185" height="190">
+    </div>
+    <div class="container pt-3 text-center">
+        <a href="#" class="text-secondary">TOKO ONLINE</a>
+    </div>
+    <!-- Link destination -->
+    <div class="container pt-3 text-center">
+        <a href="#" class="btn btn-success btn-lg btn-block" rel="noopener" aria-hidden="true" >WhatsApp</a>
+    </div>
+    <div class="container pt-3 text-center">
+        <a href="#" class="btn btn-success btn-lg btn-block"  rel="noopener">Shoope</a>
+    </div>
+    <div class="container pt-3 text-center">
+        <a href="#" class="btn btn-success btn-lg btn-block"  rel="noopener">Tokopedia</a>
+    </div>
+    <!-- Link destination -->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- responsif iklan -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2871628321156742"
+     data-ad-slot="5815407385"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    
+    <!-- Footer -->
+<footer class="page-footer font-small special-color-dark pt-4">
+
+<!-- Footer Elements -->
+<div class="container">
+
+  <!-- Social buttons -->
+  <ul class="list-unstyled list-inline text-center">
+    <li class="list-inline-item">
+      <a href="https://facebook.com/N.alam12" target="_blank" class="btn-floating btn-fb mx-1 text-dark">
+        <i class="fab fa-facebook-f"> </i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a href="https://twitter.com/alam_rmdhn" target="_blank" class="btn-floating btn-tw mx-1 text-dark">
+        <i class="fab fa-twitter"> </i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a href="https://instagram.com/alam_ramdhani" target="_blank" class="btn-floating btn-instagram mx-1 text-dark">
+        <i class="fab fa-instagram"> </i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a href="https://www.linkedin.com/in/nur-alam-ramdhani/" target="_blank" class="btn-floating btn-li mx-1 text-dark">
+        <i class="fab fa-linkedin-in"> </i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a href="https://www.github.com/alamramdhani" target="_blank" class="btn-floating btn-github mx-1 text-dark">
+        <i class="fab fa-github"> </i>
+      </a>
+    </li>
+  </ul>
+  <!-- Social buttons -->
+
+</div>
+<!-- Footer Elements -->
+
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2019 Copyright:
+  <a href="https://kujangtekno.com" class="text-dark"> Kujangtekno.com</a>
+</div>
+<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+</body>
+</html>
